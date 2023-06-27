5eed5ab2377a2d177803693dd541da394d8b2a34
cel8 <alex.celotti@gmail.com>
2023-06-27 16:00:42 -0400

    Add category search filter

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -8,11 +8,17 @@ import pizzaMargheritaSbagliata from '@assets/pizza-margherita-sbagliata.jpg';
 import pizzaScarpetta from '@assets/pizza-scarpetta.jpg';
 import uniqid from 'uniqid';
 
+const categoryList = {
+  all: 'all',
+  classic: 'classic',
+  special: 'special'
+};
+
 const productsList = [
   {
     id: `pizza_${uniqid()}`,
     to: 'product/1',
-    category: 'classic',
+    category: categoryList['classic'],
     name: 'Calzone Classico',
     description: 'Folded pizza with fiordilatte cheese, San Marzano tomato, prosciutto cotto (cooked ham), basil and Evo oil.',
     price: 8.00,
@@ -21,7 +27,7 @@ const productsList = [
   {
     id: `pizza_${uniqid()}`,
     to: 'product/2',
-    category: 'classic',
+    category: categoryList['classic'],
     name: 'Capricciosa',
     description: 'San Marzano tomato, fiordilatte cheese, capers, Cetara anchovy fillets, mushrooms, prosciutto cotto (cooked ham), Paestum artichokes, mushrooms, Caiazzo olives, oregano, Evo oil.',
     price: 9.00,
@@ -30,7 +36,7 @@ const productsList = [
   {
     id: `pizza_${uniqid()}`,
     to: 'product/3',
-    category: 'classic',
+    category: categoryList['classic'],
     name: 'Margherita DOP',
     description: 'San Marzano DOP tomato, Campana DOP buffalo mozzarella, basil and EVO oil.',
     price: 8.50,
@@ -39,7 +45,7 @@ const productsList = [
   {
     id: `pizza_${uniqid()}`,
     to: 'product/4',
-    category: 'classic',
+    category: categoryList['classic'],
     name: 'Marinara',
     description: 'San Marzano tomato, oregano, garlic and EVO oil.',
     price: 6.00,
@@ -48,7 +54,7 @@ const productsList = [
   {
     id: `pizza_${uniqid()}`,
     to: 'product/5',
-    category: 'special',
+    category: categoryList['special'],
     name: 'Sfizio ai pomodori',
     description: 'Campana DOP buffalo mozzarella, yellow datterino tomatoes, sun dried tomatoes, 24-month Grana Padano DOP cheese, EVO oil.',
     price: 12.00,
@@ -57,7 +63,7 @@ const productsList = [
   {
     id: `pizza_${uniqid()}`,
     to: 'product/6',
-    category: 'special',
+    category: categoryList['special'],
     name: 'Sapori del Cilento',
     description: 'Campana DOP buffalo mozzarella, buffalo ricotta cheese, Paestum artichokes, EVO oil.',
     price: 12.00,
@@ -66,7 +72,7 @@ const productsList = [
   {
     id: `pizza_${uniqid()}`,
     to: 'product/7',
-    category: 'special',
+    category: categoryList['special'],
     name: 'Margherita Sbagliata',
     description: 'Campana DOP Buffalo mozzarella cheese, purée of "riccio" tomatoes, basil reduction, Evo oil.',
     price: 10.00,
@@ -75,7 +81,7 @@ const productsList = [
   {
     id: `pizza_${uniqid()}`,
     to: 'product/8',
-    category: 'special',
+    category: categoryList['special'],
     name: 'Scarpetta',
     description: 'Campana DOP Buffalo mozzarella, 12-month Grana Padano fondue, uncooked tomato compote, freeze-dried pesto basil, flakes of 24-month Grana Padano DOP cheese.',
     price: 12.00,
@@ -83,4 +89,4 @@ const productsList = [
   }
 ];
 
-export { productsList };
+export { categoryList, productsList };
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,25 +1,50 @@
-import { productsList } from '@components/ProductsList';
+import { categoryList, productsList } from '@components/ProductsList';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
 import { useCartContext } from '@components/CartProvider';
 import '@styles/Shop.css';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 
 const Shop = () => {
   const contextValue = useCartContext();
+
+  const [category, setCategory] = useState(categoryList['all']);
   
   const onClickAddItem = useCallback((product) => {
     contextValue.add(product);
   }, [contextValue]);
 
+  const enableCategoryButton = useCallback((categoryName) =>{
+    return category === categoryName;
+  }, [category]);
+
+  const onCategoryClick = useCallback((categoryName) => {
+    setCategory(categoryName);
+  }, []);
+
   return (
     <div>
       <h1>Shop</h1>
+      <div className="categoryContainer">
+        <button disabled={enableCategoryButton(categoryList['all'])} 
+                onClick={() => onCategoryClick(categoryList['all'])}>
+          {categoryList['all']}
+        </button>
+        <button disabled={enableCategoryButton(categoryList['classic'])} 
+                onClick={() => onCategoryClick(categoryList['classic'])}>
+          {categoryList['classic']}
+        </button>
+        <button disabled={enableCategoryButton(categoryList['special'])} 
+                onClick={() => onCategoryClick(categoryList['special'])}>
+          {categoryList['special']}
+        </button>
+      </div>
       <div className="catalogContainer">
       {
         productsList.map((product) => {
           return (
+            ((product.category === category) || (category === categoryList['all'])) &&
             <div className="productContainer" key={product.id}>
               <Link to={product.to}>
                 <div className="productItem">
